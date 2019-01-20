0a51ea78314289b25791268dc529574d3de298a9
josue38 <jcr.clb@gmail.com>
2019-01-20 16:00:36 +0100

    yo

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -13,8 +13,7 @@ import {SharePage} from "../pages/share/share";
   templateUrl: 'app.html'
 })
 export class MyApp {
-  //rootPage:any = HomePage;
-  rootPage:any = SharePage;
+  rootPage:any = HomePage;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
