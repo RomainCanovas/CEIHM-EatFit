cb3e73a889438a3a2ba9a2da21a5c2f07e2b36dd
dallanoe <enzo.dalla-nora@etu.unice.fr>
2019-01-20 23:17:34 +0100

    weight now show an updateable graph

--- app/src/pages/weight/weight/weight.ts
+++ app/src/pages/weight/weight/weight.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
 import {AlertController, IonicPage, NavController, NavParams, ToastController} from 'ionic-angular';
+
+import { Chart } from 'chart.js';
 
 /**
  * Generated class for the WeightPage page.
@@ -15,11 +17,100 @@ import {AlertController, IonicPage, NavController, NavParams, ToastController} f
 })
 export class WeightPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public alertCtrl: AlertController, public toastCtrl: ToastController) {
+  @ViewChild('lineChart') lineChart;
+
+  text;
+
+  public weight : any = {
+    "weight" : [
+      {
+        'weight' : '80.2',
+        'date' : '15/01/2019'
+      },
+      {
+        'weight' : '81.7',
+        'date' : '16/01/2019'
+      },
+      {
+        'weight' : '80.9',
+        'date' : '17/01/2019'
+      },
+      {
+        'weight' : '79.8',
+        'date' : '18/01/2019'
+      },
+      {
+        'weight' : '79.7',
+        'date' : '19/01/2019'
+      },
+      {
+        'weight' : '78',
+        'date' : '20/01/2019'
+      }
+    ]
+  };
+  public lineChartEl : any;
+  public chartValues : any = [];
+  public chartLabels : any = [];
+
+  ionViewDidLoad()
+  {
+    this.defineChartData();
+    this.createLineChart();
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad WeightPage');
+  defineChartData()
+  {
+    let k : any;
+
+    for(k in this.weight.weight)
+    {
+      var value = this.weight.weight[k];
+
+      this.chartLabels.push(value.date);
+      this.chartValues.push(value.weight);
+    }
+
+    this.text = this.weight.weight[k].weight;
+  }
+
+  createLineChart()
+  {
+    this.lineChartEl 			= new Chart(this.lineChart.nativeElement,
+      {
+        type: 'line',
+        data: {
+          labels: this.chartLabels,
+          datasets: [{
+            label                 : 'Weight progression',
+            data                  : this.chartValues,
+            duration              : 2000,
+            easing                : 'easeInQuart',
+            fill 				   : false
+          }]
+        },
+        options : {
+          maintainAspectRatio: false,
+          legend         : {
+            display     : false
+          },
+          scales: {
+            yAxes: [{
+              ticks: {
+                stepSize: 5
+              }
+            }],
+            xAxes: [{
+              ticks: {
+                autoSkip: false
+              }
+            }]
+          }
+        }
+      });
+  }
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, public alertCtrl: AlertController, public toastCtrl: ToastController) {
   }
 
   showWeight() {
@@ -51,9 +142,21 @@ export class WeightPage {
     try{
       this.checkNewWeight(data.weight);
 
-      console.log('dlk');
+      this.text = data.weight;
+
+      var today = new Date();
+      var day = today.getDate();
+      var month = today.getMonth() + 1; //January is 0!
+      var year = today.getFullYear();
+
+      var fullDay = '' + day + '/' + month + '/' + year;
+
+      this.lineChartEl.data.labels.push(fullDay);
+      this.lineChartEl.data.datasets.forEach((dataset) => {
+        dataset.data.push(data.weight);
+      });
+      this.lineChartEl.update();
       this.showSuccessToast(data.weight);
-      console.log('dlk');
     } catch (e) {
       this.showErrorToast(e.message);
     }
@@ -72,7 +175,7 @@ export class WeightPage {
 
   private showSuccessToast(weight) {
     const toast = this.toastCtrl.create({
-      message: 'Votre nouveau poids est ' + weight + ' kg',
+      message: 'Votre nouveau poids est ' + this.text + ' kg',
       duration: 3000,
       showCloseButton: true,
       closeButtonText: 'x',
